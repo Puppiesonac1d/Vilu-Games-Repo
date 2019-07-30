93f19f018a88d3138c6fb296952bd666bcf68516
Ethese <f.goyeneche.camus@gmail.com>
2019-07-30 14:55:54 -0400

    Event Manager

---
+++ Vilu-Games-Repositorio/Assets/Scripts/EventManager/EventManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class EventManager : MonoBehaviour
+{
+    #region Variables
+    //Referencias
+    public Player player;
+
+    //Triggers
+    bool hasRadio;
+
+    //Eventos
+    public UnityEvent radioAuto;
+    #endregion
+
+    void Update()
+    {
+        EventoRadioAuto();
+    }
+
+    void EventoRadioAuto()
+    {
+        hasRadio = player.hasRadio;
+        if (hasRadio == true)
+        {
+            radioAuto.Invoke();
+            Debug.Log("Evento 1 disparado");
+        }
+    }
+}
