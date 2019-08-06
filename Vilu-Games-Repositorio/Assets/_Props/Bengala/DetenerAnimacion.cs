a278c91224fb6a074cac84150b117631a9327dfc
Puppiesonac1d <the_sorr0w@hotmail.com>
2019-08-06 12:50:43 -0400

    Script Bengala

---
+++ Vilu-Games-Repositorio/Assets/_Props/Bengala/DetenerAnimacion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetenerAnimacion: MonoBehaviour
+{
+    public Animation _animation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animation = this.gameObject.GetComponent<Animation>();
+        if(_animation == null)
+        {
+            Debug.LogWarning("Animación es nulll");
+        }
+        _animation.Play("Bengala");
+             _animation["Bengala"].speed = 0;
+
+    }
+
+}
