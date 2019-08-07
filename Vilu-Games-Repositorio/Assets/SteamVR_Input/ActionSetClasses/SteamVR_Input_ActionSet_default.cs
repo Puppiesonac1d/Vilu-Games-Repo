70440bc1e3c31d2acb6726edd81e59d6c7c611c1
Ethese <f.goyeneche.camus@gmail.com>
2019-08-07 18:57:20 -0400

    touchpadMovement

--- Vilu-Games-Repositorio/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Vilu-Games-Repositorio/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -89,6 +89,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Vector2 TouchpadMovement
+        {
+            get
+            {
+                return SteamVR_Actions.default_TouchpadMovement;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
